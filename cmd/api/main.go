1e5cdf89c8fe5ed4bb16f3c6fa60c607c4bd4baf
Masoud <masoudblackhat007@gmail.com>
2025-12-23 12:43:24 +0330

    learn: print values without manual string conversion

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
 	var message string = "my first go program"
 	age := 1
 
-	ageText := strconv.Itoa(age)
-	fmt.Println(message + " " + ageText)
+	fmt.Printf("%s %d\n", message, age)
 }
